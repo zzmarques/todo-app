96f574589c039a3a13ef49d7cd27f3770c404a99
zzmarques <walacemarqs@gmail.com>
2025-06-16 13:20:51 -0300

    feat: add Mark

--- src/components/CreateTodo/index.tsx
+++ src/components/CreateTodo/index.tsx
@@ -1,14 +1,12 @@
 import "../../sass/components/_CreateTodo.scss";
+import Mark from "../Mark";
 
 
 const CreateTodo = () => {
 
     return (
         <section className="container-create-todo">
-            <div className="mark">
-
-            </div>
-
+            <Mark />
             <div className="container-input">
                 <input type="text" name="todo" id="todo" placeholder="Create a new todo.."/>
             </div>
--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,12 +1,13 @@
 import { TfiClose } from "react-icons/tfi";
 import "../../sass/components/_Todo.scss";
+import Mark from "../Mark";
 
 
 const Todo = () => {
     return (
         <div className="todo">
             <div className="container-mtn">
-                <div className="mark"></div>
+                <Mark />
 
                 <div className="container-name">
                     <span className="todo-name">Complete online JavaScript course</span>
