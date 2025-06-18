36c8921aac4fe3d2dd71d7c1d73390d1987d6a72
zzmarques <walacemarqs@gmail.com>
2025-06-18 10:17:47 -0300

    feat: lógica para mostrar quando estiver marcado

--- src/components/Mark/index.tsx
+++ src/components/Mark/index.tsx
@@ -1,11 +1,19 @@
 import { GrFormCheckmark } from "react-icons/gr";
 import "../../sass/components/_Mark.scss";
+import { useState } from "react";
 
 
 const Mark = () => {
+
+    const [mark, setMark] = useState<boolean>(false);
+
+    const toggleMark = () => {
+        setMark(prev => !prev)
+    };
+
     return (
-        <div className="mark">
-            
+        <div className={`mark ${mark && 'active'}`} onClick={toggleMark}>
+            {mark && <GrFormCheckmark/>}
         </div>
     );
 };
